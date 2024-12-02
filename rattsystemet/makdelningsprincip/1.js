00e5967807e455c54135e5ecf916133c5bf69e4d
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2024-12-02 22:37:44 +0100

    Add files via upload

---
+++ rattsystemet/makdelningsprincip/1.js
@@ -0,0 +1,20 @@
+window.local_zaccordion_0_data = [
+{
+ auto: "1",
+ speed: "1",
+ timeout: "2",
+ trigger: "1"
+},
+{
+ image: "maktdeliningsprincip_htm_files/31.jpg"
+},
+{
+ image: "maktdeliningsprincip_htm_files/32.jpg"
+},
+{
+ image: "maktdeliningsprincip_htm_files/33.jpg"
+},
+{
+ image: "maktdeliningsprincip_htm_files/34.jpg"
+}
+];
