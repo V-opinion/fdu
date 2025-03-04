f74410f4d50678c25612acd76e74c4fa01a67713
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-03-04 18:00:33 +0100

    Add files via upload

---
+++ eu-medborgarinitiativ/Swexit-eller-krig/1.js
@@ -0,0 +1,20 @@
+window.local_zaccordion_0_data = [
+{
+ auto: "1",
+ speed: "1",
+ timeout: "2",
+ trigger: "1"
+},
+{
+ image: "Swexit-eller-krig_htm_files/166.jpg"
+},
+{
+ image: "Swexit-eller-krig_htm_files/167.jpg"
+},
+{
+ image: "Swexit-eller-krig_htm_files/168.jpg"
+},
+{
+ image: "Swexit-eller-krig_htm_files/169.jpg"
+}
+];
---
+++ eu-medborgarinitiativ/Swexit-eller-krig/2.js
@@ -0,0 +1,20 @@
+window.local_zaccordion_1_data = [
+{
+ auto: "1",
+ speed: "1",
+ timeout: "2",
+ trigger: "1"
+},
+{
+ image: "Swexit-eller-krig_htm_files/207.jpg"
+},
+{
+ image: "Swexit-eller-krig_htm_files/208.jpg"
+},
+{
+ image: "Swexit-eller-krig_htm_files/209.jpg"
+},
+{
+ image: "Swexit-eller-krig_htm_files/210.jpg"
+}
+];
