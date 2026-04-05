eeaa4ab78f6db88642acdbfab3e34a8e31ecee61
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-04-06 01:49:25 +0200

    Add files via upload

---
+++ how-to-multireligionval/30-ars-regeln/1.js
@@ -0,0 +1,27 @@
+window.local_cycleBottomradio_v8_6_data = [
+{
+ bulletimage: "fns%20rollspel_htm_files/414.png",
+ image: "fns%20rollspel_htm_files/415.jpg",
+ link: "#"
+},
+{
+ image: "fns%20rollspel_htm_files/416.png",
+ link: "#"
+},
+{
+ image: "fns%20rollspel_htm_files/417.jpg",
+ link: "#"
+},
+{
+ image: "fns%20rollspel_htm_files/418.jpg",
+ link: "#"
+},
+{
+ image: "fns%20rollspel_htm_files/419.jpg",
+ link: "#"
+},
+{
+ image: "fns%20rollspel_htm_files/420.jpg",
+ link: "#"
+}
+];
